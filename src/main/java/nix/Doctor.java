0d69c5774570a9e0b1e4455d5e2d341e7f05ab77
MarinaFedorenko <marina.fedora28@gmail.com>
2020-06-30 00:46:17 +0300

    Add

---
+++ src/main/java/nix/Doctor.java
@@ -0,0 +1,15 @@
+package nix;
+
+public class Doctor {
+
+    public void startAppointment(Patient patient){
+        System.out.println("Кабинет имеется");
+        System.out.println("Дожидается пациента");
+        System.out.println("Спрашивает о симптомах");
+        System.out.println("Отвечает на вопросы");
+        System.out.println("Проверяет показатели здоровья");
+        System.out.println("Отвечает на вопросы");
+        System.out.println("Прописывает лечение");
+    }
+
+}
---
+++ src/main/java/nix/Patient.java
@@ -0,0 +1,4 @@
+package nix;
+
+public class Patient {
+}
